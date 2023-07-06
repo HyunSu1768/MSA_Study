0f436d268a5687141a2a679e88c022b372638b86
HyunSu1768 <azxcv1768@gmail.com>
2023-07-06 19:42:33 +0900

    ✨ :: Add Membership Entity

--- membership-service/src/main/java/org/example/membership/domain/Membership.java
+++ membership-service/src/main/java/org/example/membership/domain/Membership.java
@@ -1,2 +1,91 @@
-package org.example.membership.domain;public class Membership {
+package org.example.membership.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Membership {
+
+    @Getter
+    private final String membershipId;
+
+    @Getter
+    private final String name;
+
+    @Getter
+    private final String email;
+
+    @Getter
+    private final String address;
+
+    @Getter
+    private final boolean isValid;
+
+    @Getter
+    private final boolean isCorp;
+
+    public static Membership generateMember(
+            MembershipId membershipId,
+            MembershipName membershipName,
+            MembershipEmail membershipEmail,
+            MembershipAddress membershipAddress,
+            MembershipIsValid membershipIsValid,
+            MembershipIsCorp membershipIsCorp
+    ){
+        return new Membership(
+                membershipId.membershipId,
+                membershipName.membershipName,
+                membershipEmail.membershipEmail,
+                membershipAddress.membershipAddress,
+                membershipIsValid.isValidValue,
+                membershipIsCorp.isCorpValue
+        );
+    }
+
+    @Value
+    public static class MembershipId{
+        public MembershipId(String value){
+            this. membershipId = value;
+        }
+        String membershipId;
+    }
+
+    @Value
+    public static class MembershipName{
+        public MembershipName(String value){
+            this.membershipName = value;
+        }
+        String membershipName;
+    }
+    @Value
+    public static class MembershipEmail{
+        public MembershipEmail(String value){
+            this.membershipEmail = value;
+        }
+        String membershipEmail;
+    }
+    @Value
+    public static class MembershipAddress{
+        public MembershipAddress(String value){
+            this.membershipAddress = value;
+        }
+        String membershipAddress;
+    }
+    @Value
+    public static class MembershipIsValid{
+        public MembershipIsValid(boolean value){
+            this.isValidValue = value;
+        }
+        boolean isValidValue;
+    }
+
+    @Value
+    public static class MembershipIsCorp{
+        public MembershipIsCorp(boolean value){
+            this.isCorpValue = value;
+        }
+        boolean isCorpValue;
+    }
+
 }
